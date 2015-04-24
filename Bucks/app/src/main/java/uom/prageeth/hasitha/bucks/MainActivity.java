9e51e19fa106876b6cef8952e54268b9ed238755
hasitha.prageeth <haztha@gmail.com>
2015-04-24 19:47:05 +0530

    Bucks App Code - 3.3
    Using material Design

--- Bucks/app/src/main/java/uom/prageeth/hasitha/bucks/MainActivity.java
+++ Bucks/app/src/main/java/uom/prageeth/hasitha/bucks/MainActivity.java
@@ -3,16 +3,23 @@ package uom.prageeth.hasitha.bucks;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new CurrencyFragment())
